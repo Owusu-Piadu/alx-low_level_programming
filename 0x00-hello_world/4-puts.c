1d6b22efe4078736911db98efb7ec1336f6ff601
Owusu-Piadu <eugeneowusupiadu@gmail.com>
2022-12-08 01:39:04 -0800

    my first C program using put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *My first C to print using puts
+ *Return: is always 0 (success)
+ */
+int main (void)
+{
+  puts("Programming is like building a multilingual puzzle \n");
+  return 0;
+}
