859e0ee8f278d5d2182d20fc56de54eb8b53cb34
Owusu-Piadu <eugeneowusupiadu@gmail.com>
2022-12-08 01:49:41 -0800

    adding puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  *My first C to print using puts
  *Return: is always 0 (success)
  */
-int main (void)
+int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+  return (0);
 }
