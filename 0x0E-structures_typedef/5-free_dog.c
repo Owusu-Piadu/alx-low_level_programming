de9e0d8a3294319a302b15093b74b7fc37c749cb
Owusu-Piadu <eugeneowusupiadu@gmail.com>
2023-01-16 23:45:19 +0000

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,36 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs.
+ * @d: struct dog.
+ *
+ * Return: no return.
+ */
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs.
+ * @d: struct dog.
+ *
+ * Return: no return.
+ */
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
